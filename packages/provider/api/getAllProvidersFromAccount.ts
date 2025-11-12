1456e912376d9bbd0ab842100b2a76ac92e15ac0
= <seila@gmail.com>
2025-11-12 16:57:18 -0300

    feat: extend GetAllProvidersFromAccount to include description and linkedAccounts in response

--- packages/provider/api/getAllProvidersFromAccount.ts
+++ packages/provider/api/getAllProvidersFromAccount.ts
@@ -1,6 +1,14 @@
-import { prisma, SocialAccount } from "@workspace/database"
+import { prisma, AccountsOnSocialAccounts } from "@workspace/database"
 import { authProcedure } from "@workspace/trpc"
 import { z } from "zod"
+
+
+const AccountOnSocialAccointSchema = z.object({
+  provider: z.string(),
+  providerAccountId: z.string(),
+  socialAccountId: z.string(),
+  assignedAt: z.date()
+})
 
 
 export const GetAllProvidersFromAccount = authProcedure.output(z.object({
@@ -10,13 +18,25 @@ export const GetAllProvidersFromAccount = authProcedure.output(z.object({
       id: z.string(),
       createdAt: z.date(),
       updatedAt: z.date(),
-      userId: z.string()
+      userId: z.string(),
+      description: z.string().nullish(),
+      linkedAccounts: z.array(AccountOnSocialAccointSchema)
+
     })
   )
 })).query(async ({ ctx: { session } }) => {
   const providersConnected = await prisma.socialAccount.findMany({
     where: {
       userId: session.user.id!
+    },
+    select: {
+      linkedAccounts: true,
+      name: true,
+      id: true,
+      description: true,
+      createdAt: true,
+      updatedAt: true,
+      userId: true
     }
   })
 
