9c504033cf1bf29b3f696ed0b8e166c52a88ba05
= <seila@gmail.com>
2025-11-16 18:30:06 -0300

    refactor(billings): remove changeUserCss and changeQuote API endpoints
    - Deleted changeUserCss.ts file
    - Deleted changeQuote.ts file
    - Removed unused imports and exports from billing router

--- packages/billings/src/api/router.ts
+++ packages/billings/src/api/router.ts
@@ -1,17 +1,15 @@
 import { router } from "@workspace/trpc";
-import { changeUserCss } from "./changeUserCss";
+
 import { quoteToday } from "./css/quote";
 import { GenerateInviteCode, GetInviteCode } from "./generateInviteCode";
 import { generatePayment } from "./generatePayment";
-import { changeQuote } from "./changeQuote";
+
 import { checkPaymentStatus } from "./checkPaymentStatus";
 
 export const billing = router({
   generatePayment,
   quoteToday,
-  changeUserCss,
   checkPaymentStatus,
   GenerateInviteCode,
   GetInviteCode,
-  changeQuote
 })
